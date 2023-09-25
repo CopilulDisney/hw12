229ef6247913c6a2d994a9ed1af9c95e937e5d72
Vasile <vasile.amethyst69@gmail.com>
2023-09-25 16:33:22 +0300

    first

---
+++ index.js
@@ -0,0 +1,41 @@
+// var camelCase = 'camelCase' //Exercitiu 1
+// var UPPER_SNAKE_CASE = 'upper snake case'
+// var Capitalcase = 'capital case'
+
+// var index = "Salut,"  //Exercitiu 2
+// var index2 = "sunt eu,"
+// var index3 = "un... haiduc."
+
+// console.log(`${index} ${index2} ${index3}`)
+
+// let num = 34;  //Exercitiu 3
+
+// console.log("Output : " + num.toString(8))
+
+// let a = 30;
+
+// console.log(String(a))
+
+// var numValue = (!1) //Exercitiu 4
+
+// console.log(numValue)
+
+// const quantity1 = "12"
+// Number(quantity)
+// const quantity = "12"
+
+// console.log(+quantity)
+// console.log(typeof Number(quantity))
+// console.log(typeof quantity1)
+
+const falsy1 = ''; //Exericitiu 5
+const falsy2 = 0;
+const falsy3 = null;
+const falsy4  = NaN;
+const falsy5 = undefined;
+
+if(true) {
+    console.log('inside block', falsy1)
+}
+else(false) 
+    console.log('outside block', falsy1)
